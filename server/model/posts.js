689a3390693a94b583baffadd1e98f6af8251da6
Andy Warr <andywarr@gmail.com>
2013-08-18 23:39:37 +0000

    Added markdown so posts can be written in markdown instead of HTML

--- server/model/posts.js
+++ server/model/posts.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var marked = require('marked');
 
 var postsdir = __dirname + '/../posts/';
 
@@ -17,14 +18,17 @@ Posts.prototype.cachePosts = function() {
     var fileStats = fs.statSync(postsdir + files[i]);
 
     // Get file content                                                   
-    var fileContent = fs.readFileSync(postsdir + files[i], 'utf8');
+    var fileContentMarkdown = fs.readFileSync(postsdir + files[i], 'utf8');
+
+    // Convert from markdown to html
+    var fileContentHTML = marked(fileContentMarkdown);
 
     // Push file info and content to feed object                          
     this.posts.posts.push({
       title: files[i],
       created: fileStats.ctime,
       modified: fileStats.mtime,
-      content: fileContent
+      content: fileContentHTML
     });
   }
 };
